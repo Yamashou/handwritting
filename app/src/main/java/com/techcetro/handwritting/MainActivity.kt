2e8fce50800d0e73df4d92973ec5db89fd69e1fc
Yamashou <1230124fw@gmail.com>
2017-11-11 12:28:43 +0900

    Add logic

--- app/src/main/java/com/techcetro/handwritting/MainActivity.kt
+++ app/src/main/java/com/techcetro/handwritting/MainActivity.kt
@@ -2,12 +2,12 @@ package com.techcetro.handwritting
 
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
-import android.view.View
 import android.view.Menu
 import android.view.MenuItem
+import kotlinx.android.synthetic.main.content_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,12 +16,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
-
+        var i:Int = 0
         val fab = findViewById(R.id.fab) as FloatingActionButton
         fab.setOnClickListener {
             val paintView = findViewById(R.id.view) as PaintView
             paintView.clear()
+            i++
+            textView.text = i.toString()
         }
+
+
+
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/techcetro/handwritting/PaintView.kt
+++ app/src/main/java/com/techcetro/handwritting/PaintView.kt
@@ -9,6 +9,7 @@ import android.view.MotionEvent
 import android.view.View
 import android.util.*
 
+
 class PaintView constructor(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
     private val paint: Paint
     private val path: Path
@@ -54,44 +55,91 @@ class PaintView constructor(context: Context, attrs: AttributeSet? = null) : Vie
                 invalidate()
             }
         }
-
-        Log.d("My app", "X="+ data[0] + " Y=" + data[1] + " Number=" + data[2])
         return true
     }
 
     fun getCharacter(){
+
         val co = count.toInt()
-        for(i in 1..co+1){
+
+        for(i in 1..co+1) {
+
             if(i.equals(co+1)){break}
+
             val angls = arrayListOf<Double>()
             val setAngls = arrayListOf(arrayListOf<Double>())
-            for(j in 1..countsNum[i-1]){
+
+            for(j in 1..countsNum[i-1]) {
+
                 val x1 = allDatas[i][j][0]
                 val x2 = allDatas[i][j+1][0]
                 val y1 = allDatas[i][j][1]
                 val y2 = allDatas[i][j+1][0]
-                if((x1.equals(x2))&&(y1.equals(y2))){continue}
+
+                if((x1.equals(x2)) && (y1.equals(y2))) {continue}
+
+                if(isSubZero(x1, x2) || isSubZero(y1, y2)){
+                    continue
+                }
+
                 val xd = getSub(x1,x2)
                 val yd = getSub(y1,y2)
                 angls.add(getAng(xd,yd))
+
                 if(j+1 == countsNum[i-1]){break}
             }
-            for(j in 0..angls.size){
+
+            for(j in 0..angls.size) {
                 Log.d("Angl app", "Set1=" + angls[j]+" Set2="+angls[j+1])
                 val set = arrayListOf<Double>(angls[j],angls[j+1])
                 setAngls.add(set)
+
                 if(j+2 == angls.size){break}
             }
         }
     }
-    fun getSub(a:Double,b:Double):Double{
-       return Math.max(a,b)-Math.min(a,b)
-
+    fun getFourClassification(a: Double, b: Double): Int {
+        if(0 <= a && 0 <= b) {
+            return 1
+        } else if( a <= 0 && 0 < b) {
+            return 4
+        } else if (0 < a && b <= 0) {
+            return 2
+        } else if(0 < a && 0 < b) {
+            return 3
+        }
+        return 0
     }
+
+
+    fun getEightClassifcation(a: Int, b:Int): Int {
+        return 4 * (a - 1) + b
+    }
+
+    fun getAngleClassification(a: Double): Int {
+        if (a < Math.PI / 8) {
+            return 1
+        } else if (Math.PI / 8 <= a && a < Math.PI / 4) {
+            return 2
+        } else if (Math.PI / 4 <= a && a < Math.PI * 3 / 4) {
+            return 3
+        } else if ( Math.PI * 3 / 4 <= a ){
+            return 4
+        }
+        return 0
+    }
+
+    fun getSub(a:Double,b:Double):Double{
+       return a - b
+    }
+
     fun getAng(a:Double,b:Double):Double{
         return Math.atan2(a,b)
     }
 
+    fun isSubZero(a:Double, b:Double):Boolean{
+        return (Math.max(a,b)-Math.min(a,b)).equals(0)
+    }
     fun clear() {
         path.reset()
         getCharacter()
